5e2b8d14227e10baabeb01cd23078a5f9bc2e6ac
Fabio Benítez Ramírez <Fabio2000br@gmail.com>
2018-10-30 09:27:03 +0100

    Ejercicio java

--- Repaso Bucles/Repaso01.java
+++ Repaso Bucles/Repaso01.java
@@ -1,14 +1,11 @@
 public class Repaso01 {
-  public static void main (String [] args ){
-  
-  System.out.println("Introduce un número inicial");
-  int numeroinicial = Integer.parseInt(System.console().readLine());
+  public static void main (String [] args ){  
+    int i = 1;
   System.out.println("Introduce el número de números que quieres que muestre por pantalla");
-  int contador = Integer.parseInt(System.console().readLine());
-  while(contador!=0){
-    System.out.println(numeroinicial);
-    numeroinicial++;
-    contador--;
+  int numero = Integer.parseInt(System.console().readLine());
+  while(i <= numero){
+    System.out.println(i);
+    i++;
     }
   }
 }
