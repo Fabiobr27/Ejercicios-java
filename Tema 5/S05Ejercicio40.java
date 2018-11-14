cbde93a399ce73e738150e61f3c2500b16b2adc1
Fabio Benítez Ramírez <Fabio2000br@gmail.com>
2018-11-14 20:33:29 +0100

    Ejercicios java

---
+++ Tema 5/S05Ejercicio40.java
@@ -0,0 +1,68 @@
+
+public class S05Ejercicio40 {
+  public static void main(String[] args) {
+
+    System.out.print("Por favor, introduzca la altura del rombo: ");
+    int alturaIntroducida = Integer.parseInt(System.console().readLine());
+
+    int espaciosInternos = 0;
+    int espaciosPorDelante = alturaIntroducida / 2;
+
+    if ((alturaIntroducida < 3) || (alturaIntroducida % 2 == 0)) {
+      System.out.print("Datos incorrectos. Debe introducir una altura impar mayor o igual a 3");
+    } else {
+      
+      int altura = 1;
+      
+      //arriba
+      while (altura <= alturaIntroducida / 2 + 1) {
+
+        // espacios 
+        for (int i = 1; i <= espaciosPorDelante; i++) {
+          System.out.print(" ");
+        }
+
+        // línea
+        System.out.print("*");
+        for (int i = 1; i < espaciosInternos; i++) {
+          System.out.print(" ");
+        }
+
+        if (altura>1) {
+          System.out.print("*");
+        }
+        
+        System.out.println();
+        altura++;
+        espaciosPorDelante--;
+        espaciosInternos+=2;
+      }
+      espaciosInternos = alturaIntroducida - 3;
+      espaciosPorDelante = 1;
+      altura = 0;
+
+      while (altura < alturaIntroducida / 2) {
+
+        // espacios 
+        for (int i = 1; i <= espaciosPorDelante; i++) {
+          System.out.print(" ");
+        }
+
+        // línea
+        System.out.print("*");
+        for (int i = 1; i < espaciosInternos; i++) {
+          System.out.print(" ");
+        }
+        
+        if(altura < alturaIntroducida / 2 - 1) {
+          System.out.print("*");
+        }
+        
+        System.out.println();
+        altura++;
+        espaciosPorDelante++;
+        espaciosInternos -= 2;
+      } 
+    } 
+  }
+}
