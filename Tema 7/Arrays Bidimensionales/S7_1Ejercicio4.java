2bfd8c51c74d7c23d1e011f3d9328dd77111204e
Fabio Benítez Ramírez <Fabio2000br@gmail.com>
2018-11-29 10:58:54 +0100

    Ejercicios java

---
+++ Tema 7/Arrays Bidimensionales/S7_1Ejercicio4.java
@@ -0,0 +1,56 @@
+public class S7_1Ejercicio4 {
+  public static void main (String [] args)
+    throws InterruptedException {
+    
+    
+  int [][] numeros = new int [4][5];
+      
+      int filas;
+      int columnas;
+      
+      System.out.println("Introduce números enteros");
+      for(filas=0;filas<4;filas++){
+        for(columnas=0;columnas<5;columnas++){
+        int aleatorios = (int)(Math.random ()* 900 + 100);
+        System.out.println(" Fila " + filas + " Columnas " + columnas + ":");
+        numeros[filas][columnas]= aleatorios;
+      }
+    }
+      
+      int sumaFilas;
+      for(filas=0;filas<4;filas++){
+      sumaFilas=0;
+      for(columnas=0;columnas<5;columnas++){
+        System.out.printf("%7d  ",numeros[filas][columnas]);
+        sumaFilas += numeros[filas][columnas];
+        Thread.sleep(100);
+      }
+      System.out.printf("|%7d\n",sumaFilas);
+      Thread.sleep(500);
+    }
+    //pinta las rayas
+    for(columnas = 0; columnas < 5; columnas++){
+      System.out.print("----------");
+    }
+    System.out.println("-----------");
+
+      
+      int sumaColumnas;
+      int sumaTotal = 0;
+      
+      for(columnas=0;columnas<5;columnas++){
+        sumaColumnas=0;
+      for(filas=0;filas<4;filas++){
+      
+      sumaColumnas += numeros[filas][columnas];
+      }
+    sumaTotal += sumaColumnas;
+    System.out.printf("%7d  ", sumaColumnas);
+    Thread.sleep(500);
+    }
+    System.out.printf("|%7d  ", sumaTotal);
+  
+  //cierra public static
+  }
+}
+  
